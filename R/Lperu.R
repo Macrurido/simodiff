396b6833d1abc14766f9b13aa36dab491a7dace9
Macrurido <hugo.aguirre@imipas.gob.mx>
2025-02-06 17:42:00 -0700

    adding Lperu data

---
+++ R/Lperu.R
@@ -0,0 +1,20 @@
+#' *Lutjanus peru* mature females
+#'
+#' The dataset contains the length and reproductive condition of female red snapper
+#' (*Lutjanus peru*) collected from the Eastern Central Pacific. The reproductive
+#' condition is categorized as follows: 0 indicates an immature or inactive mature
+#' organism, while 1 denotes an actively mature organism. The variables are as follows:
+#'
+#' @format A data frame with 917 rows and 3 variables:
+#' \describe{
+#'   \item{sex}{The sex of the organism, Female, Male or Combined ("Female")}
+#'   \item{Length}{length of fish in cm (6--65)}
+#'   \item{Binomial}{reproductive condition (0--1)}
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Lperu
+#' @usage data(Lperu)
+#' @format A data frame with 917 rows and 2 variables
+NULL
